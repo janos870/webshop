ed137112f15c75b0fd41aaf8f7110d2932bb2f4b
Janos Balogh <116517053+janos870@users.noreply.github.com>
2023-12-31 15:53:45 +0100

    add detail view page

--- shop/views.py
+++ shop/views.py
@@ -12,8 +12,13 @@ def index(request):
         product_objects = product_objects.filter(title__icontains=item_name)
 
     # paginator code
-    paginator = Paginator(product_objects, 4)
+    paginator = Paginator(product_objects, 16)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
 
     return render(request, 'shop/index.html', {'product_objects': product_objects})
+
+
+def detail(request, id):
+    product_object = Products.objects.get(id=id)
+    return render(request, 'shop/detail.html', {'product_object': product_object})
